0e41dca7588fe8ee083eae5575bb04f4b8401ac3
mAyty <github.oil306@passmail.net>
2026-02-11 21:13:48 +0200

    better rate limit handling

--- ai_artist_detector/domain/youtube.py
+++ ai_artist_detector/domain/youtube.py
@@ -26,6 +26,7 @@ class YouTubeAdapterService:
         self.youtube_handles_repository = youtube_handles_repository
         self.youtube_music_aliases_repository = youtube_music_aliases_repository
         self.youtube_search_results_repository = youtube_search_results_repository
+        self.failed_rate_limit_count = 0
 
     def get_artist_id_from_handle(self, artist_handle: str) -> str | None:
         artist_handle = artist_handle.removeprefix('@')
@@ -38,10 +39,16 @@ class YouTubeAdapterService:
             logger.debug('UsingCachedYoutubeId', artist_handle=artist_handle, youtube_id=artist_id)
             return artist_id
 
+        if self.failed_rate_limit_count:
+            self.failed_rate_limit_count += 1
+            logger.error('RateLimitExceeded', artist_handle=artist_handle)
+            return None
+
         try:
             artist_id = self.youtube_client.conver_youtube_handle_to_id(artist_handle)
         except RateLimitExceededError:
             logger.error('RateLimitExceeded', artist_handle=artist_handle)
+            self.failed_rate_limit_count += 1
             return None
         except RuntimeError:
             logger.exception('FailedToFetchYoutubeId', artist_handle=artist_handle)
@@ -77,10 +84,16 @@ class YouTubeAdapterService:
             logger.debug('UsingCachedSearchQuery', search_query=search_query, artist_ids=artist_ids)
             return artist_ids
 
+        if self.failed_rate_limit_count:
+            logger.error('RateLimitExceeded', search_query=search_query)
+            self.failed_rate_limit_count += 1
+            return set()
+
         try:
             artist_ids = self.youtube_client.find_artist_by_search_query(search_query)
         except RateLimitExceededError:
             logger.error('SearchRateLimitExceeded', search_query=search_query)
+            self.failed_rate_limit_count += 1
             return set()
         self.youtube_search_results_repository.set_artist_ids(search_query, artist_ids)
         return artist_ids
